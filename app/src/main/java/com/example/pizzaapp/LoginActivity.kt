59d405976163dbc8768d1b69bc4e05f1544b798d
amikom <pamel4515@gmail.com>
2023-11-22 15:07:35 +0700

    up pizza terbaru

--- app/src/main/java/com/example/pizzaapp/LoginActivity.kt
+++ app/src/main/java/com/example/pizzaapp/LoginActivity.kt
@@ -33,29 +33,29 @@ class LoginActivity : AppCompatActivity() {
         btnLogin.setOnClickListener {
             //object class databaseHelper
             val databaseHelper = DatabaseHelper(this)
+            /*
+                        //check data
+                        val data:String = databaseHelper.checkData("pamela@amikom.ac.id")
+                        Toast.makeText(this@LoginActivity, "Result : " + data,
+                                Toast.LENGTH_SHORT).show()
 
-            //check data
-            val data:String = databaseHelper.checkData("pamela@amikom.ac.id")
-            Toast.makeText(this@LoginActivity, "Result : " + data,
-                    Toast.LENGTH_SHORT).show()
-            if (data == null){
-                //insert data
-                databaseHelper.addAccount("pamela@amikom.ac.id",
-                    "Pamela Riva Adina", "Kasir", "12345")
-            }
-            //instance DatabaseHelper
-            val dbHelper = DatabaseHelper(this)
-            dbHelper.addAccount("pamela@amikom.ac.id",
-                "Pamela Riva Adina", "Kasir", "12345")
-
+                        if (data == null){
+                            //insert data
+                            databaseHelper.addAccount("pamela@amikom.ac.id",
+                                "Pamela Riva Adina", "Kasir", "12345")
+                        }
+                        instance DatabaseHelper
+                        val dbHelper = DatabaseHelper(this)
+                        dbHelper.addAccount("pamela@amikom.ac.id",
+                            "Pamela Riva Adina", "Kasir", "12345")
+            */
             val email = txtUsername.text.toString().trim()
             val password = txtPassword.text.toString().trim()
 
             //check login
             val result:Boolean = databaseHelper.checkLogin(email, password)
             if (result == true) {
-                Toast.makeText(this@LoginActivity, "Login Succes ",
-                Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@LoginActivity, "Login Succes ",Toast.LENGTH_SHORT).show()
                 val intentLogin = Intent(this@LoginActivity,
                     MainActivity::class.java)
                     startActivity(intentLogin)
